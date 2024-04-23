14a8bb6e5bac757fadc3955e9a454314f2acb286
abrauf18 <abrauf.dev@gmail.com>
2024-04-23 15:20:07 +0500

    feat: forgot password screen

--- app/components/modules/auth/ForgetPassword.tsx
+++ app/components/modules/auth/ForgetPassword.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import loginImg from "@/app/assets/images/login.png";
+import RightsideImg from "@/app/assets/images/login.png";
 import { Input } from "@/app/components/ui/input"
 import { Label } from "@/app/components/ui/label"
 
@@ -7,9 +7,9 @@ import { Label } from "@/app/components/ui/label"
 export default function ForgetPassword() {
   return (
     <div
-      className="flex md:ml-4 md:w-full mt-6 w-screen h-full md:h-screen xl:h-full rounded-3xl justify-center items-center bg-cover bg-no-repeat md:bg-center"
+      className="flex md:w-full mt-6 w-screen h-full md:h-screen xl:h-full rounded-3xl justify-center items-center bg-cover bg-no-repeat md:bg-center"
       style={{
-        backgroundImage: `url(${loginImg.src})`,
+        backgroundImage: `url(${RightsideImg.src})`,
       }}
     >
       <div className="flex w-[85%] md:w-full py-6 justify-center items-center">
@@ -18,7 +18,6 @@ export default function ForgetPassword() {
           <p className="md:w-full text-center mb-6 text-sm ">
             Reset your password by providing your registered email address
           </p>
-          <hr className="my-6" />
           <div className="grid mb-4 w-full items-center gap-1.5 	">
             <Label htmlFor="email" className="md:text-lg text-sm font-semibold">Email Address</Label>
             <Input
---
+++ app/forget-password/page.tsx
@@ -0,0 +1,24 @@
+import ForgetPassword from "../components/modules/auth/ForgetPassword";
+import Navbar from "@/app/components/shared/Navbar";
+import AuthLeftSide from "@/app/components/shared/AuthLeftSide";
+import leftSideImg from "@/app/assets/images/loginLeftSide.jpg";
+
+export default function page() {
+  return (
+    <>
+      <Navbar
+        buttonText="Register Now"
+        textBeforeButton="Don't have an account?"
+      />
+      <div className="grid xl:grid-cols-2 md:grid-cols-1 sm:grid-cols-1 gap-4 md:mx-6 md:p-4">
+        <AuthLeftSide
+          heading1="YOUR HUB"
+          heading2="FOR FINDING THE BEST"
+          heading3="SERVICE PROVIDERS"
+          url={leftSideImg}
+        />
+        <ForgetPassword />
+      </div>
+    </>
+  );
+}
