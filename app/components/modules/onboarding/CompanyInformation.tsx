1e9c65f0a3a7377cff3b1b6edc3e72a73c690971
abrauf18 <abrauf.dev@gmail.com>
2024-04-24 11:07:44 +0500

    feat: complete company information page

--- app/components/modules/onboarding/CompanyInformation.tsx
+++ app/components/modules/onboarding/CompanyInformation.tsx
@@ -5,7 +5,7 @@ import companyIcon from "@/app/assets/icons/company.svg"
 import { Input } from "@/app/components/ui/input"
 import { Label } from "@/app/components/ui/label"
 import { Button } from "@/app/components/ui/button"
-import { UserRound } from 'lucide-react';
+import uploadIcon from '@/app/assets/icons/upload.svg'
 import user from '@/app/assets/icons/user.svg'
 import building from '@/app/assets/icons/building.svg'
 import phone from '@/app/assets/icons/phone.svg'
@@ -14,10 +14,10 @@ import location from '@/app/assets/icons/location.svg'
 
 const CompanyInformation = () => {
   return (
-    <div className='flex flex-col justify-center items-center'>
-      <h1 className='xl:font-semibold lg:text-2xl md:font-semibold md:text-3xl text-center'>Welcome Aboard! Let's Complete Your Profile.</h1>
+    <div className='flex flex-col justify-center items-center my-20 mx-10'>
+      <h1 className='font-semibold lg:text-2xl md:text-3xl text-2xl text-center'>Welcome Aboard! Let's Complete Your Profile.</h1>
       <p className='lg:text-lg md:text-lg md:font-medium text-center text-gray-400 mt-6'>Please Provide required Information about your company</p>
-      <div className='flex mt-10 gap-6 text-lg'>
+      <div className='flex flex-col md:flex-row mt-10 gap-6 text-lg'>
         <div className='flex items-center '>
           <Image src={companyIcon} alt="Company Icon"
             className='w-[4rem] h-[4rem]' />
@@ -29,8 +29,20 @@ const CompanyInformation = () => {
           <span className='text-gray-400'>Subscription Plan</span>
         </div>
       </div>
-      <div className='flex flex-col w-[60%] justify-center items-center'>
+      <div className='flex flex-col md:w-[60%] w-full justify-center items-center'>
         <form className="bg-white w-full shadow-md rounded-3xl px-8 pt-6 pb-8 my-10">
+          <div className="mb-4">
+            <Label
+              htmlFor="companyName"
+              className="md:text-lg text-sm font-semibold"
+            >
+              Upload Logo
+            </Label>
+            <div className="w-full h-40 border-dashed border-2 border-gray-300 rounded-2xl flex flex-col justify-center items-center">
+              <Image src={uploadIcon} alt="Upload Logo" className="w-8 h-8" />
+              <h5 className="text-lg font-medium mt-3 text-gray-700">Upload Logo</h5>
+            </div>
+          </div>
           <div className="mb-4 w-full relative">
             <Label
               htmlFor="companyName"
@@ -127,15 +139,15 @@ const CompanyInformation = () => {
             </div>
           </div>
         </form>
-        <div className="flex w-full items-center justify-end mt-6 gap-6">
+        <div className="flex w-full items-center md:justify-end justify-center mt-2 gap-6">
           <button
-            className="bg-gray-300 text-white font-bold py-2 px-4 rounded-3xl"
+            className="w-full lg:w-[10rem] bg-gray-300 font-medium py-3 px-10 rounded-3xl"
             type="submit"
           >
             Cancel
           </button>
           <button
-            className="bg-primary  text-white font-bold py-2 px-4 rounded-3xl"
+            className="w-full lg:w-[10rem] bg-primary text-white font-medium py-3 px-10 rounded-3xl"
             type="submit"
           >
             Next
