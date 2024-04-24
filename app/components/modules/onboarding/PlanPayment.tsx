07c64cd068dcba8b540bec501d19f9ee92ef9445
abrauf18 <abrauf.dev@gmail.com>
2024-04-24 17:12:55 +0500

    feat: subscription plan payment screen

--- app/components/modules/onboarding/PlanPayment.tsx
+++ app/components/modules/onboarding/PlanPayment.tsx
@@ -22,16 +22,16 @@ const PlanPayment = () => {
         </div>
       </div>
       <div className='flex flex-col md:w-[60%] w-full justify-center items-center'>
-        <div className="w-full px-8 pt-6 pb-8 my-10 gap-6">
+        <div className="w-full md:px-8 px-4 pt-6 pb-6  gap-6">
           <RegisteredCards
             cardType='VISA'
-            cardOwner='Jane copper'
+            cardOwner='Jane Copper'
             expiryDate='12/34'
             cvv='123'
           />
           <NewCard />
         </div>
-        <div className="flex w-full items-center md:justify-end justify-center mt-2 gap-6">
+        <div className="flex w-full items-center md:justify-end justify-center gap-6 px-4">
           <button
             className="w-full lg:w-[10rem] bg-gray-300 font-medium py-3 px-10 rounded-3xl"
             type="submit"
--- app/components/modules/onboarding/RegisteredCard.tsx
+++ app/components/modules/onboarding/RegisteredCard.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import cardCheck from "@/app/assets/icons/cardcheck.svg"
 import card from "@/app/assets/icons/card.svg"
 import Image from 'next/image';
+import e from 'express';
 
 interface RegisteredCard {
   cardType: string;
@@ -12,25 +13,54 @@ interface RegisteredCard {
 
 const RegisteredCards: React.FC<RegisteredCard> = ({ cardType, cvv, cardOwner, expiryDate }) => {
   return (
-    <div className="flex flex-col bg-white w-full shadow-md rounded-3xl px-8 pt-6 pb-8 my-10">
-      <h1 className="text-xl font-bold mb-4">Registered Cards</h1>
+    <div className="flex flex-col bg-white w-full shadow-md rounded-3xl pt-6 pb-8 md:my-10">
+      <div className='flex w-full items-center px-8'>
+        <Image src={card} alt='card' className='h-6 w-6 mr-2' />
+        <span className="font-semibold text-base md:text-lg">Registered Cards</span>
+      </div>
+      <hr className='w-full my-6' />
+      <div className='flex md:gap-24 gap-8 items-center w-full text-xs md:text-base px-4 md:px-8'>
+        <div className='flex flex-col gap-y-4'>
+          <Image src={cardCheck} alt='cardcheck'
+            className='md:h-6 h-5' />
+          <Image src={card} alt='card'
+            className='md:h-5 h-5' />
+        </div>
+        <div className='flex flex-col gap-y-4'>
+          <h1 className='font-semibold'>Card</h1>
+          <span className='Rubik'>{cardType}</span>
+        </div>
+        <div className='flex flex-col gap-y-4'>
+          <h1 className='font-semibold'>CVV</h1>
+          <span className='Rubik'>{cvv}</span>
+        </div>
+        <div className='flex flex-col gap-y-4'>
+          <h1 className='font-semibold'>Card owner</h1>
+          <span className='Rubik'>{cardOwner}</span>
+        </div>
+        <div className='flex flex-col gap-y-4'>
+          <h1 className='font-semibold'>Expiry date</h1>
+          <span className='Rubik'>{expiryDate}</span>
+        </div>
+      </div>
 
-      <ul className="flex items-center w-full justify-between text-base font-semibold ">
-        <li className="p-2"><Image src={cardCheck} alt='cardcheck' className='h-7 w-7' /></li>
-        <li >Card</li>
-        <li >CVV</li>
-        <li >Card Owner</li>
-        <li >Expiry Date</li>
-      </ul>
-      <ul className="flex items-center w-full justify-between text-center text-base font-semibold pr-6">
-        <li className="p-2"><Image src={card} alt='card' className='h-7 w-7' /></li>
-        <li className='pr-6'>{cardType}</li>
-        <li className='pr-6'>{cvv}</li>
-        <li className='pr-8'>{cardOwner}</li>
-        <li >{expiryDate}</li>
-      </ul>
+      {/* <div className="flex flex-col justify-center items-center gap-4">
+        <ul className="flex items-center w-full gap-24 text-base font-semibold ">
+          <li className="p-2"><Image src={cardCheck} alt='cardcheck' className='h-7 w-7' /></li>
+          <li >Card</li>
+          <li >CVV</li>
+          <li >Card Owner</li>
+          <li>Expiry Date</li>
+        </ul>
+        <ul className="flex  items-center  w-full gap-24 text-center text-base font-semibold rubik ">
+          <li className="p-2"><Image src={card} alt='card' className='h-7 w-7' /></li>
+          <li >{cardType}</li>
+          <li >{cvv}</li>
+          <li >{cardOwner}</li>
+          <li >{expiryDate}</li>
+        </ul>
+      </div> */}
     </div>
-
   );
 };
 
