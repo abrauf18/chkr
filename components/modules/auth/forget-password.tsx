6b65d57463ef56627fbd222c0eb5a6b12e83db79
abrauf18 <abrauf.dev@gmail.com>
2024-04-29 20:46:38 +0500

    feat: validation for reset and forget password

--- components/modules/auth/forget-password.tsx
+++ components/modules/auth/forget-password.tsx
@@ -1,9 +1,22 @@
-import React from "react";
+"use client"
+import React, { useState } from "react";
 import RightsideImg from "@/public/images/login.png";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { ForgetPasswordSchema } from "@/lib/schema";
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 
 export default function ForgetPassword() {
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(ForgetPasswordSchema),
+  });
+
   return (
     <div
       className="flex md:w-full mt-6 w-screen h-full md:h-screen xl:h-full rounded-3xl justify-center items-center bg-cover bg-no-repeat md:bg-center"
@@ -12,7 +25,8 @@ export default function ForgetPassword() {
       }}
     >
       <div className="flex w-[85%] md:w-full py-6 justify-center items-center">
-        <form className="bg-white shadow-md rounded-3xl px-8 pt-6 pb-8 mb-4">
+        <form className="bg-white shadow-md rounded-3xl px-8 pt-6 pb-8 mb-4"
+          onSubmit={handleSubmit((d) => console.log(d))}>
           <h2 className="text-center md:text-2xl text-xl md:font-medium font-bold	mb-6">
             Forgot Password
           </h2>
@@ -24,11 +38,14 @@ export default function ForgetPassword() {
               Email Address
             </Label>
             <Input
+              {...register("password")}
               className="bg-[#F9F8F8]"
               type="email"
               id="email"
               placeholder="Email"
             />
+            {errors.email && <p className="text-red-600 mt-2">Email is required</p>}
+            {errors.email && errors.email.type === "pattern" && <p className="text-red-600 mt-2">Invalid email format</p>}
           </div>
           <div className="flex items-center justify-center mt-6">
             <button
