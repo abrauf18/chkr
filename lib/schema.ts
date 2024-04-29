54958e9477feb8821519a588b764786f75d33eb1
abrauf18 <abrauf.dev@gmail.com>
2024-04-29 20:45:56 +0500

    feat: add schema for reset and forget password

--- lib/schema.ts
+++ lib/schema.ts
@@ -1,4 +1,4 @@
-import { z } from "zod";
+import { z, ZodType } from "zod";
 
 export const SignUpSchema = z.object({
   firstname: z.string().min(2).max(50).min(1),
@@ -17,3 +17,38 @@ export const LoginSchema = z.object({
     .min(8, { message: "Password must contain at least 8 characters" })
 });
 
+export const ForgetPasswordSchema = z.object({
+  email: z.string().email().min(1),
+});
+
+// interface ResetPasswordFormData {
+//   password: string;
+//   retypePassword: string;
+// }
+
+// export const ResetPasswordSchema = z.object({
+//   password: z.string()
+//     .min(8, { message: "Password must contain at least 8 characters" }),
+//   retypePassword: z.string()
+//     .refine((value, data: ResetPasswordFormData) => value === data.password, {
+//       message: "Passwords don't match",
+//       path: ["retypePassword"],
+//     })
+// });
+
+
+interface FormData {
+  password: string;
+  confirmPassword: string;
+}
+
+export const ResetPasswordSchema = z.object({
+  password: z
+    .string()
+    .min(8, { message: "Password is too short" })
+    .max(20, { message: "Password is too long" }),
+  confirmPassword: z.string(),
+}).refine((data: FormData) => data.password === data.confirmPassword, {
+  message: "Passwords do not match",
+  path: ["confirmPassword"], // path of error
+});
