d7c6915ebb2ed3c9a80dee9340359ae04132445d
abrauf18 <abrauf.dev@gmail.com>
2024-04-29 19:25:43 +0500

    feat: define schema for signup and login form

---
+++ lib/schema.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+export const SignUpSchema = z.object({
+  firstname: z.string().min(2).max(50).min(1),
+  lastname: z.string().min(2).max(50).min(1),
+  email: z.string().email().min(1),
+  password: z.string().min(8).min(1),
+  confirmPassword: z.string().min(8).min(1),
+}).refine(data => data.password === data.confirmPassword, {
+  message: "Passwords do not match",
+  path: ["confirmPassword"]
+});
+
+export const LoginSchema = z.object({
+  email: z.string().email().min(1),
+  password: z.string().min(8).min(1),
+});
+
