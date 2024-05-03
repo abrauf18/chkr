845db0ecafa33c5c8811af64b7ce9ad8caebc352
abrauf18 <abrauf.dev@gmail.com>
2024-05-03 21:49:44 +0500

    fix: design for sidebar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,25 +1,40 @@
+"use client";
+
 import React from "react";
 import Sidebar from "@/components/shared/sidebar";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 interface RootLayoutProps {
   children: React.ReactNode;
 }
 
 const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
+  const [open, setOpen] = React.useState(true);
   return (
-    <html lang="en">
-      <body>
-        <main className="flex flex-col md:flex-row">
-          <div>
-            <Sidebar />
-          </div>
-          <div className="lg:basis-full w-full pr-6 md:pr-4 pt-2 lg:pr-6">
-            {children}
-          </div>
-        </main>
-      </body>
-    </html>
+    <div className="grid w-full grid-cols-12 grid-rows-6 h-full">
+      <nav
+        className={`relative h-full w-full row-span-6 xl:px-6 lg:px-4 md:px-3 col-start-1 border-r-2 bg-black text-white ${
+          open ? "col-span-3 col-end-3" : "col-span-1 col-end-1"
+        }`}
+      >
+        <Sidebar open={open} />
+        <button
+          className="absolute lg:-right-4 md:-right-6 top-6 rounded-full p-1.5 bg-white text-black"
+          onClick={() => setOpen(!open)}
+        >
+          {open ? <ChevronLeft size={20} /> : <ChevronRight size={20} />}
+        </button>
+      </nav>
+      <main
+        className={`no-scrollbar px-8 ${
+          open ? "col-start-3" : "col-start-2"
+        } col-span-full row-span-full overflow-auto`}
+      >
+        {children}
+      </main>
+    </div>
   );
 };
 
 export default RootLayout;
+
--- components/shared/sidebar.tsx
+++ components/shared/sidebar.tsx
@@ -1,56 +1,71 @@
-"use client";
-import { Sidebar, Menu, MenuItem, SubMenu } from 'react-pro-sidebar';
-import React from 'react';
-import { Settings } from 'lucide-react';
-import { Users } from 'lucide-react';
-import { StickyNote } from 'lucide-react';
-import DashboardSquare from '@/assets/icons/dashboard-square';
-import { ChevronLeft } from 'lucide-react';
-import SidebarLogo from '@/assets/icons/sidebar-logo';
-import SupportIcon from '@/assets/icons/support-icon';
-import clsx from 'clsx';
-import { AlignJustify } from 'lucide-react';
+import React from "react";
+import { Users, StickyNote, Settings, CircleHelp } from "lucide-react";
+import DashboardSquare from "@/assets/icons/dashboard-square";
+import SidebarLogo from "@/assets/icons/sidebar-logo";
+import LogoFooter from "@/assets/icons/footer-logo";
+import Link from "next/link";
+import clsx from "clsx";
 
-const SideBar: React.FC = () => {
-  const [collapsed, setCollapsed] = React.useState(false);
-  const [toggled, setToggled] = React.useState(false);
-  const [broken, setBroken] = React.useState(window.matchMedia('(max-width: 800px)').matches);
-
+const SideBar = ({ open }: { open: boolean }) => {
+  const list = [
+    {
+      title: "Dashboard",
+      icon: <DashboardSquare />,
+      path: "/dashboard",
+    },
+    {
+      title: "All Jobs",
+      icon: <StickyNote />,
+      path: "/all-jobs",
+    },
+    {
+      title: "Employees",
+      icon: <Users />,
+      path: "/employees",
+    },
+    {
+      title: "Settings",
+      icon: <Settings />,
+      path: "/settings",
+    },
+  ];
 
   return (
-    <div>
-      <Sidebar
-        collapsed={collapsed} collapsedWidth="100px" transitionDuration={1000}
-        onBackdropClick={() => setToggled(false)} toggled={toggled} customBreakPoint="800px" onBreakPoint={setBroken}
-        backgroundColor='black'
-        className='overflow-visible md:w-[90px] h-full lg:w-fit md:p-3 top-0 left-0 '>
-        <Menu>
-          <MenuItem icon={<SidebarLogo />} className='mb-16 mt-6 text-white hover:bg-black font-medium text-2xl'>CHKR</MenuItem>
-          <MenuItem icon={<DashboardSquare />} className={clsx('text-white hover:text-orange-500 hover:bg-white', { 'text-orange-500 bg-white': collapsed })}>Dashboard</MenuItem>
-          <MenuItem icon={<StickyNote />} className={clsx('text-white hover:text-orange-500 hover:bg-white', { 'text-orange-500 bg-white': collapsed })}>All Jobs</MenuItem>
-          <MenuItem icon={<Users />} className={clsx('text-white hover:text-orange-500 hover:bg-white', { 'text-orange-500 bg-white': collapsed })} >Employees</MenuItem>
-          <MenuItem icon={<Settings />} className={clsx('text-white hover:text-orange-500 hover:bg-white', { 'text-orange-500 bg-white': collapsed })}>Settings</MenuItem>
-        </Menu>
-
-        <div className='bg-red-500 overflow-visible mx-[210px] my-auto z-50 shadow-lg'>
-          <button className="sb-button" onClick={() => setCollapsed(!collapsed)}>
-            <div className='bg-white flex items-center justify-center rounded-full h-8 w-8'><ChevronLeft /></div>
-          </button>
+    <>
+      <div className="w-full flex gap-x-4 justify-start py-6">
+        <Link href="#">
+          <div className="font-garamond text-2xl font-bold hover:text-sky-400">
+            <div className="logo logo-triangle relative w-10 h-10 inline-block mt-6">
+              {open ? <LogoFooter /> : <SidebarLogo />}
+            </div>
+          </div>
+        </Link>
+      </div>
+      <div className="relative w-full space-y-6 h-[85%] mt-12">
+        <div className="space-y-3 h-[76%] overflow-auto section-scrollbar">
+          {list.map((item) => (
+            <div>
+              <Link href={item.path}>
+                <div
+                  className={clsx(
+                    "flex items-center w-full h-12 hover:text-primary hover:bg-white rounded-3xl xl:px-4",
+                    !open && "justify-center"
+                  )}
+                >
+                  <div>{item.icon}</div>
+                  {open && <span className="ml-3">{item.title}</span>}
+                </div>
+              </Link>
+            </div>
+          ))}
         </div>
-
-        <div className='mt-96'>
-          <SupportIcon />
+        <div className="flex items-center justify-center w-12 h-12 p-2 bg-primary rounded-full hover:animate-bounce cursor-pointer">
+          <CircleHelp />
         </div>
-      </Sidebar>
-      <main style={{ display: 'flex', padding: 10 }}>
-        <div>
-          <button className="sb-button text-black" onClick={() => setToggled(!toggled)}>
-            <AlignJustify />
-          </button>
-        </div>
-      </main>
-    </div>
+      </div>
+    </>
   );
 };
 
 export default SideBar;
+
