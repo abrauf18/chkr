d7482164f561767a934ec5d211b89a63ff644b09
abrauf18 <abrauf.dev@gmail.com>
2024-05-14 17:34:59 +0500

    feat: schema for employee form

--- lib/types.ts
+++ lib/types.ts
@@ -69,3 +69,10 @@ export const ResetPasswordSchema = z
     path: ["confirmPassword"], // path of error
   });
 
+export const EmployeeSchema = z.object({
+  employeeName: z.string().max(50).min(1, { message: "Employee name must not be empty" }),
+  email: z.string().email().min(1, { message: "Email must not be empty" }),
+  phoneNumber: z
+    .string()
+    .min(8, { message: "Phone Number must contain at least 8 numbers" }),
+});
