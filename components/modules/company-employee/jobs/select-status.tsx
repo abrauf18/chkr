a704600417c575c3cc6e1e3bf4c35efd077675a3
abrauf18 <abrauf.dev@gmail.com>
2024-05-17 19:31:02 +0500

    feat: select status component

---
+++ components/modules/company-employee/jobs/select-status.tsx
@@ -0,0 +1,47 @@
+// use client
+import React, { useState } from 'react';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@/components/ui/select';
+import { History } from 'lucide-react';
+import Checkin from '@/assets/icons/checkin';
+import Checkout from '@/assets/icons/checkout';
+
+export default function SelectStatus() {
+  const [selected, setSelected] = useState<'checkin' | 'checkout'>('checkin');
+
+  const handleValueChange = (newValue: 'checkin' | 'checkout') => {
+    setSelected(newValue);
+  };
+
+  const getColor = () => {
+    switch (selected) {
+      case 'checkin':
+        return 'bg-blue-400 color-white';
+      case 'checkout':
+        return 'bg-red-500';
+      default:
+        return '';
+    }
+  };
+
+  return (
+    <Select value={selected} onValueChange={handleValueChange}>
+      <SelectTrigger className={`w-[9rem] rounded-2xl text-white ${getColor()}`}>
+        <SelectValue />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="checkin">
+          <div className='flex gap-2 items-center justify-center'>
+            <Checkin />
+            <span>Checkin</span>
+          </div>
+        </SelectItem>
+        <SelectItem value="checkout">
+          <div className='flex gap-2 items-center justify-center'>
+            <Checkout />
+            <span>Checkout</span>
+          </div>
+        </SelectItem>
+      </SelectContent>
+    </Select>
+  );
+}
