d35a42147a048023ed0ae46ee2cf7437266255f2
abrauf18 <abrauf.dev@gmail.com>
2024-05-17 17:39:52 +0500

    feat: header and rendering of cards

---
+++ components/modules/company-employee/jobs/job-requests.tsx
@@ -0,0 +1,11 @@
+import DashboardHeader from '@/components/shared/dashboard-header'
+import React from 'react'
+
+export default function JobRequests() {
+  return (
+    <div>
+      <DashboardHeader
+        title='My Jobs' />
+    </div>
+  )
+}
