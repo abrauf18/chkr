8f1c12cd7342c5e06f90d8aeba05d0fa5736f24c
abrauf18 <abrauf.dev@gmail.com>
2024-05-20 03:07:45 +0500

    feat: mapped job request cards and pagination

--- app/(dashboard)/company-employee/jobs/page.tsx
+++ app/(dashboard)/company-employee/jobs/page.tsx
@@ -1,6 +1,8 @@
+import CompletedJobs from '@/components/modules/company-employee/jobs/completed-jobs'
+import JobRequests from '@/components/modules/company-employee/jobs/job-requests'
 import MyJobs from '@/components/modules/company-employee/jobs/my-jobs'
 import React from 'react'
 
 export default function page() {
-  return <MyJobs />
+  return <JobRequests />
 }
