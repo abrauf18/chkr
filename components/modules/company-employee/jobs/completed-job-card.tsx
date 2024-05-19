47005c6305e8635bf56aafa8396ac76798e58629
abrauf18 <abrauf.dev@gmail.com>
2024-05-20 01:56:00 +0500

    feat: dynamic completed-job-card

---
+++ components/modules/company-employee/jobs/completed-job-card.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+import Image from "next/image";
+import Location from "@/assets/icons/location-icon";
+import EditIcon from "@/assets/icons/edit-icon";
+import DeleteIcon from "@/assets/icons/delete-icon";
+
+
+interface CompletedJobCardProps {
+  userName: string;
+  location: string;
+  status: string;
+  zipCode: string;
+  dateTime: string;
+  service: string;
+  paymentStatus: String;
+  payment: string;
+}
+
+const CompletedJobCard: React.FC<CompletedJobCardProps> = ({
+  userName,
+  location,
+  status,
+  zipCode,
+  dateTime,
+  service,
+  paymentStatus,
+  payment,
+}) => {
+  return (
+    <div className="mt-4 bg-white rounded-3xl py-10 px-4">
+      <div className="flex flex-wrap justify-between">
+        <div className="flex flex-col">
+          <h1 className="font-bold text-xl mb-3">{userName}</h1>
+          <div className="flex items-center">
+            <Location className="h-6 w-6" />
+            <span className="font-semibold text-lg text-gray-700 ml-2">
+              {location}
+            </span>
+          </div>
+        </div>
+        <div className="flex gap-2 h-3/4 mt-4 lg:mt-0">
+          <div className="flex items-center bg-gray-100 rounded-xl p-3">
+            <div className="bg-green-500 rounded-full h-2 w-2 mr-2"></div>
+            <span>{status}</span>
+          </div>
+        </div>
+      </div>
+      <div className="flex mt-6 justify-between lg:flex-row flex-col lg:gap-0 gap-5">
+        <div className="flex flex-wrap gap-2 xl:gap-8 ">
+          <div className="flex flex-col text-sm lg:text-lg whitespace-nowrap">
+            <span className="font-bold md:text-lg">Zip Code:</span>
+            <span className="bg-gray-100 rounded-2xl py-3 px-6 mt-2 md:text-base">
+              {zipCode}
+            </span>
+          </div>
+          <div className="flex flex-col text-sm lg:text-lg whitespace-nowrap">
+            <span className="font-bold md:text-lg">Date & Time:</span>
+            <span className="bg-gray-100 rounded-2xl py-3 px-6 mt-2 md:text-base">
+              {dateTime}
+            </span>
+          </div>
+          <div className="flex flex-col text-sm whitespace-nowrap">
+            <span className="font-bold md:text-lg">Service:</span>
+            <span className="bg-gray-100 rounded-2xl py-3 px-6 mt-2 md:text-base">
+              {service}
+            </span>
+          </div>
+          <div className="flex flex-col text-sm whitespace-nowrap">
+            <span className="font-bold md:text-lg">Payment:</span>
+            <span className="bg-gray-100 rounded-2xl py-3 px-6 mt-2 md:text-base flex items-center">
+              <div className="bg-green-500 rounded-full h-2 w-2 mr-2"></div>
+              {paymentStatus}
+            </span>
+          </div>
+        </div>
+        <span className="font-bold text-3xl mt-3 whitespace-nowrap">$ {payment} USD</span>
+      </div>
+    </div>
+  );
+};
+
+export default CompletedJobCard;
+
