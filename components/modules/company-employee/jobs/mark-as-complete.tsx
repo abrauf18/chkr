7253d41e77acf9dddb6b835757ac847ddd53a1ba
abrauf18 <abrauf.dev@gmail.com>
2024-05-20 04:48:58 +0500

    feat: complete and cancel buttons function

--- components/modules/company-employee/jobs/mark-as-complete.tsx
+++ components/modules/company-employee/jobs/mark-as-complete.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react';
 import {
   Dialog,
   DialogContent,
@@ -6,18 +7,30 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
-import { Check } from 'lucide-react'
-import { Button } from '@/components/ui/button'
-
+} from '@/components/ui/dialog';
+import { Check } from 'lucide-react';
+import { Button } from '@/components/ui/button';
 
 export default function MarkAsComplete() {
+  const [isCompleted, setIsCompleted] = useState(false);
+
+  const handleMarkComplete = () => {
+    setIsCompleted(true);
+  };
+
+  const handleCancel = () => {
+    setIsCompleted(false); // Reset completion status on cancel
+  };
+
   return (
     <>
       <Dialog>
-        <DialogTrigger className='bg-gray-200 flex items-center px-4 py-2 text-white rounded-3xl gap-1'>
+        <DialogTrigger
+          className={`bg-gray-200 flex items-center px-4 py-2 text-white rounded-3xl gap-1 ${isCompleted ? 'bg-green-500' : ''
+            }`}
+        >
           <Check />
-          <span>Mark as Complete</span>
+          <span>{isCompleted ? 'Completed' : 'Mark as Complete'}</span>
         </DialogTrigger>
         <DialogContent className='bg-white'>
           <DialogHeader>
@@ -29,16 +42,15 @@ export default function MarkAsComplete() {
             </DialogDescription>
           </DialogHeader>
           <div className='flex justify-between mt-4'>
-            <Button className='bg-transparent border border-primary rounded-3xl hover:bg-primary text-primary'>
+            <Button className='bg-transparent border border-primary rounded-3xl hover:bg-primary text-primary hover:text-white' onClick={handleCancel}>
               Cancel
             </Button>
-            <Button className='bg-green-500 text-white rounded-3xl hover:bg-green-500'>
+            <Button className='bg-green-500 text-white rounded-3xl hover:bg-green-500' onClick={handleMarkComplete}>
               Mark as Complete
             </Button>
           </div>
         </DialogContent>
       </Dialog>
-
     </>
-  )
+  );
 }
