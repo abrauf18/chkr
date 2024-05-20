45e8aafd5923d2a4f9d6ad568a55208b308e01a4
abrauf18 <abrauf.dev@gmail.com>
2024-05-20 18:49:42 +0500

    feat: add text editor

--- components/modules/company-employee/jobs/job-details.tsx
+++ components/modules/company-employee/jobs/job-details.tsx
@@ -1,40 +1,35 @@
-'use client'
-import { Button } from '@/components/ui/button'
-import { Bold, Italic, Link2, MapPinned } from 'lucide-react'
-import React, { useState } from 'react'
-import Comment, { CommentProps } from '../../company-admin/jobs/comments'
-import Select from './select-status'
-import MarkAsComplete from './mark-as-complete'
+"use client";
+
+import { MapPinned } from "lucide-react";
+import React, { useRef, useState } from "react";
+import Comment, { CommentProps } from "../../company-admin/jobs/comments";
+import Select from "./select-status";
+import MarkAsComplete from "./mark-as-complete";
+import TextEditor from "@/components/shared/text-editor";
 
 export default function JobDetails() {
-  const [isBold, setIsBold] = useState(false);
-  const [isItalic, setIsItalic] = useState(false);
+  const [text, setText] = useState("");
 
-  const handleBoldClick = () => {
-    setIsBold(!isBold);
+  const handleChange = (event: any) => {
+    setText(event.target.value);
   };
-
-  const handleItalicClick = () => {
-    setIsItalic(!isItalic);
-  };
-
   const comments: CommentProps[] = [
     {
-      date: '27/03/2023',
-      time: '03:34 pm',
-      content: 'Try to cover tasks asap, we’ve alot of more jobs todo!',
-      user: 'You'
+      date: "27/03/2023",
+      time: "03:34 pm",
+      content: "Try to cover tasks asap, we’ve alot of more jobs todo!",
+      user: "You",
     },
     {
-      date: '28/03/2023',
-      time: '04:45 am',
-      content: 'Tasks completed on time. Ready for next tasks.',
-      user: 'Employee'
+      date: "28/03/2023",
+      time: "04:45 am",
+      content: "Tasks completed on time. Ready for next tasks.",
+      user: "Employee",
     },
   ];
 
   return (
-    <div className='flex flex-col px-4 gap-4 w-full'>
+    <div className="flex flex-col px-4 gap-4 w-full">
       <div className="flex flex-wrap justify-between">
         <div className="flex flex-col">
           <h1 className="font-bold text-xl mb-3">Leslie Alexander</h1>
@@ -50,9 +45,15 @@ export default function JobDetails() {
           <MarkAsComplete />
         </div>
       </div>
-      <div className='flex flex-col gap-1'>
-        <span className='font-bold text-lg'>Description:</span>
-        <p>Mollit in laborum tempor Lorem incididunt irure. Aute eu ex ad sunt. Pariatur sint culpa do incididunt eiusmod eiusmod culpa. laborum tempor Lorem incididunt. Sed fermentum eget velit sit amet sagittis. Sed egestas egestas arcu, quis fermentum justo laoreet non. Maecenas sapien quam, mollis vitae blandit a, blandit vel lectus.</p>
+      <div className="flex flex-col gap-1">
+        <span className="font-bold text-lg">Description:</span>
+        <p>
+          Mollit in laborum tempor Lorem incididunt irure. Aute eu ex ad sunt.
+          Pariatur sint culpa do incididunt eiusmod eiusmod culpa. laborum
+          tempor Lorem incididunt. Sed fermentum eget velit sit amet sagittis.
+          Sed egestas egestas arcu, quis fermentum justo laoreet non. Maecenas
+          sapien quam, mollis vitae blandit a, blandit vel lectus.
+        </p>
       </div>
       <div className="flex flex-wrap gap-6">
         <div className="flex flex-col text-sm lg:text-lg whitespace-nowrap">
@@ -81,46 +82,18 @@ export default function JobDetails() {
         </div>
       </div>
       <div>
-        <span className='font-bold text-lg'>Map Direction</span>
+        <span className="font-bold text-lg">Map Direction</span>
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15282225.79979123!2d73.7250245393691!3d20.750301298393563!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30635ff06b92b791%3A0xd78c4fa1854213a6!2sIndia!5e0!3m2!1sen!2sin!4v1587818542745!5m2!1sen!2sin"
           width="100%"
           height="200"
-          style={{ border: 0, borderRadius: '1rem' }}
+          style={{ border: 0, borderRadius: "1rem" }}
           aria-hidden="false"
         ></iframe>
       </div>
       <div>
-        <span className='font-bold text-lg'>Onsite Progress:</span>
-        <div className='border border-gray-200 rounded-lg'>
-          <textarea
-            id="onsite-progress"
-            name="onsite-progress"
-            placeholder='Write your onsite progress here.'
-            rows={4}
-            cols={50}
-            maxLength={500}
-            style={{ resize: 'none', fontWeight: isBold ? 'bold' : 'normal', fontStyle: isItalic ? 'italic' : 'normal' }}
-            className='p-3 outline-none focus:outline-none w-full rounded-lg'
-          ></textarea>
-          <hr className='my-2 w-[95%] mx-auto' />
-          <div className='flex justify-between items-center mx-3 my-3'>
-            <div className='flex'>
-              <Button onClick={handleBoldClick}>
-                <Bold color='gray' />
-              </Button>
-              <Button onClick={handleItalicClick}>
-                <Italic color='gray' />
-              </Button>
-              <Button>
-                <Link2 color='gray' />
-              </Button>
-            </div>
-            <div>
-              <Button className='rounded-3xl text-white px-6'>Share</Button>
-            </div>
-          </div>
-        </div>
+        <span className="font-bold text-lg">Onsite Progress:</span>
+        <TextEditor />
         {/* Map comments */}
         {comments.map((comment, index) => (
           <Comment key={index} {...comment} />
@@ -129,3 +102,4 @@ export default function JobDetails() {
     </div>
   );
 }
+
