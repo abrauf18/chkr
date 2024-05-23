5b954b853f305225a015f21e694172308e0a2b5a
abrauf18 <abrauf.dev@gmail.com>
2024-05-23 10:16:59 +0500

    feat: payment module

---
+++ components/modules/company-employee/payment/payments.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import EarningsOverview from './earnings-overview'
+import DashboardHeader from '@/components/shared/dashboard-header'
+import { TransactionHistory } from './transaction-history'
+import { Button } from '@/components/ui/button'
+import { CalendarDays, ChevronDown } from 'lucide-react'
+
+export default function Payments() {
+  return (
+    <div className='flex flex-col gap-4'>
+      <DashboardHeader
+        title='Payments' />
+      <div className='flex justify-between items-center'>
+        <h1 className='text-base md:text-xl font-base'>My Earnings Overview</h1>
+        <div className="flex justify-center items-center gap-2">
+          <Button className="md:w-[90%] w-1/2 bg-white hover:bg-white rounded-3xl p-6 text-sm lg:text-base mobile:hidden">
+            <CalendarDays className='mr-2' color='#FF2600' />
+            March 11 - March 17, 2024
+            <ChevronDown className='ml-2' />
+          </Button>
+          <Button className="w-full md:w-[90%] bg-white rounded-3xl p-6 text-sm lg:text-base">
+            Filter
+            <ChevronDown className="ml-2 md:w-[1rem] md:h-[1rem] w-[1rem] h-[1rem]" />
+          </Button>
+        </div>
+      </div>
+      <EarningsOverview />
+      <div className='flex justify-between items-center'>
+        <h1 className='text-base md:text-xl font-base'>Transaction History</h1>
+        <div className="flex justify-center items-center gap-2">
+          <Button className="md:w-[90%] w-1/2 bg-white hover:bg-white rounded-3xl p-6 text-sm lg:text-base mobile:hidden">
+            <CalendarDays className='mr-2' color='#FF2600' />
+            March 11 - March 17, 2024
+            <ChevronDown className='ml-2' />
+          </Button>
+          <Button className="w-full md:w-[90%] bg-white rounded-3xl p-6 text-sm lg:text-base">
+            Filter
+            <ChevronDown className="ml-2 md:w-[1rem] md:h-[1rem] w-[1rem] h-[1rem]" />
+          </Button>
+        </div>
+      </div>
+      <TransactionHistory />
+    </div>
+  )
+}
