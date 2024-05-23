e73a333d0a6335d3cfba0a33d022fdb6759fe6a5
abrauf18 <abrauf.dev@gmail.com>
2024-05-23 10:31:05 +0500

    fix: add use client on top

--- components/shared/show-job-details.tsx
+++ components/shared/show-job-details.tsx
@@ -1,4 +1,6 @@
-import React from 'react'
+"use client";
+
+import React from "react";
 import {
   Dialog,
   DialogContent,
@@ -6,9 +8,9 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
-import JobDetails from './job-details'
-import { CircleArrowRight, Eye } from 'lucide-react'
+} from "@/components/ui/dialog";
+import JobDetails from "./job-details";
+import { CircleArrowRight, Eye } from "lucide-react";
 import { usePathname } from "next/navigation";
 
 export default function ShowJobDetails() {
@@ -17,22 +19,23 @@ export default function ShowJobDetails() {
   return (
     <Dialog>
       <DialogTrigger>
-        {pathname === "/company-employee/jobs" &&
-          <div className='flex gap-2 items-center'>
-            <span className='text-base'>View Details</span>
-            <CircleArrowRight className='w-4 h-4' />
-          </div>}
-        {pathname === "/company-admin/jobs" &&
+        {pathname === "/company-employee/jobs" && (
+          <div className="flex gap-2 items-center">
+            <span className="text-base">View Details</span>
+            <CircleArrowRight className="w-4 h-4" />
+          </div>
+        )}
+        {pathname === "/company-admin/jobs" && (
           <div className="flex items-center justify-center rounded-lg h-10 w-10 bg-green-100">
             <Eye color="#27AE60" />
-          </div>}
+          </div>
+        )}
       </DialogTrigger>
-      <DialogContent
-        className="bg-white md:max-w-[65%] xl:max-w-[50%] mobile:max-w-[90%] max-h-[80vh] overflow-y-auto overflow-x-hidden rounded-3xl"
-      >
+      <DialogContent className="bg-white md:max-w-[65%] xl:max-w-[50%] mobile:max-w-[90%] max-h-[80vh] overflow-y-auto overflow-x-hidden rounded-3xl">
         <DialogHeader>
-          <DialogTitle>Job Details
-            <hr className='my-6' />
+          <DialogTitle>
+            Job Details
+            <hr className="my-6" />
           </DialogTitle>
           <DialogDescription>
             <JobDetails />
@@ -40,6 +43,6 @@ export default function ShowJobDetails() {
         </DialogHeader>
       </DialogContent>
     </Dialog>
-
-  )
+  );
 }
+
