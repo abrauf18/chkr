a7d138c2a136b155d0fa3191cab57af727181a7e
abrauf18 <abrauf.dev@gmail.com>
2024-05-23 12:16:42 +0500

    feat: add header

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -1,76 +1,29 @@
-// use client (assuming you're using it for code splitting)
-import React from "react";
-import Image from "next/image";
-
-interface TabData {
-  name: string;
-  email: string;
-}
+import React from 'react';
+import { Calendar, ChevronDown } from 'lucide-react';
+import { Button } from '../ui/button';
 
 interface HeaderProps {
-  activeTab: string;
-  setActiveTab: (tab: string) => void;
-  personalData: TabData;
-  companyData: TabData;
-  isEmployee?: boolean;
+  title: string;
 }
 
-const Header: React.FC<HeaderProps> = ({
-  activeTab,
-  setActiveTab,
-  personalData,
-  companyData,
-  isEmployee,
-}) => {
-  const activeData = activeTab === "personal" ? personalData : companyData;
-
+export default function Header({ title }: HeaderProps) {
   return (
-    <div className="max-w-screen-lg mx-auto mt-8 bg-white p-4 rounded-2xl">
-      <div className="relative">
-        <div className="bg-[url('/images/Profile_bg.svg')] h-32 rounded-2xl"></div>
-        <div className="absolute -bottom-8 left-1/2 transform -translate-x-1/2 w-24 h-24 bg-white rounded-full border-4 border-white">
-          <Image
-            src="/images/companyLogo.svg"
-            alt="company logo"
-            width={5}
-            height={5}
-            className="w-full h-full rounded-full"
-          />
+    <>
+      <div className='flex justify-between items-center'>
+        <h1 className='text-xl font-base'>{title}</h1>
+        <div className="flex justify-center items-center gap-2">
+          <Button className="md:w-[90%] w-1/2 bg-white rounded-3xl p-6 text-sm lg:text-base">
+            Select Date
+            <Calendar className='ml-2' />
+          </Button>
+          <Button className="md:w-[90%] w-1/2 bg-white rounded-3xl p-6 text-sm lg:text-base">
+            Filter
+            <ChevronDown
+              className="ml-2 md:w-[1rem] md:h-[1rem] w-[1rem] h-[1rem]"
+            />
+          </Button>
         </div>
       </div>
-      <div className="mt-12 text-center">
-        <h1 className="text-xl font-semibold">{activeData?.name}</h1>
-        <p className="text-gray-500">{activeData?.email}</p>
-      </div>
-      {!isEmployee && (
-        <div className="mt-6 flex justify-center">
-          <div className="flex space-x-8">
-            <button
-              onClick={() => setActiveTab("personal")}
-              className={`pb-2 ${
-                activeTab === "personal"
-                  ? "border-b-4 border-red-500 font-bold"
-                  : "font-medium"
-              }`}
-            >
-              Personal Information
-            </button>
-            <button
-              onClick={() => setActiveTab("company")}
-              className={`pb-2 ${
-                activeTab === "company"
-                  ? "border-b-4 border-red-500 font-bold"
-                  : "font-medium"
-              }`}
-            >
-              Company Information
-            </button>
-          </div>
-        </div>
-      )}
-    </div>
+    </>
   );
-};
-
-export default Header;
-
+}
