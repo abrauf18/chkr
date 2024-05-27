1be8ae2ebd04ed5ec51ac5306ea004fcb086da4f
abrauf18 <abrauf.dev@gmail.com>
2024-05-27 15:02:50 +0500

    fix: changed styling of dynamic header

--- components/shared/header.tsx
+++ components/shared/header.tsx
@@ -9,15 +9,16 @@ interface HeaderProps {
 export default function Header({ title }: HeaderProps) {
   return (
     <>
-      <div className='flex justify-between items-center'>
-        <h1 className='text-xl font-base'>{title}</h1>
-        <div className="flex justify-center items-center gap-2">
-          <Button className="md:w-[90%] w-1/2 bg-white hover:bg-white rounded-3xl p-6 text-sm lg:text-base mobile:hidden">
+      <div className='flex mobile:flex-col justify-between items-center'>
+        <h1 className='text-xl font-base whitespace-nowrap w-full'>{title}</h1>
+        <div className="flex items-center gap-2 mobile:w-full mobile:mt-2">
+          <Button className="md:w-[90%] bg-white hover:bg-white rounded-3xl p-6 text-sm lg:text-base">
             <CalendarDays className='mr-2' color='#FF2600' />
-            March 11 - March 17, 2024
+            <span className='mobile:hidden'>March 11 - March 17, 2024</span>
+            <span className='md:hidden'>Select Date</span>
             <ChevronDown className='ml-2' />
           </Button>
-          <Button className="w-full md:w-[90%] bg-white rounded-3xl p-6 text-sm lg:text-base">
+          <Button className="md:w-[90%] bg-white rounded-3xl p-6 text-sm lg:text-base">
             Filter
             <ChevronDown className="ml-2 md:w-[1rem] md:h-[1rem] w-[1rem] h-[1rem]" />
           </Button>
