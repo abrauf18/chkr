816c47336e1e8d93c2e6096b32ecd0371ed3c7d2
abrauf18 <abrauf.dev@gmail.com>
2024-06-03 11:01:57 +0500

    perf: remove text editor and add text area of shadcn

--- components/shared/job-details.tsx
+++ components/shared/job-details.tsx
@@ -7,8 +7,9 @@ import React, { useState } from "react";
 import Comment, { CommentProps } from "../modules/company-admin/jobs/comments";
 import Select from "../modules/company-employee/jobs/select-status";
 import MarkAsComplete from "../modules/company-employee/jobs/mark-as-complete";
-import TextEditor from "@/components/shared/text-editor";
 import { usePathname } from "next/navigation";
+import { Textarea } from "../ui/textarea";
+import { Button } from "../ui/button";
 
 export default function JobDetails() {
   const pathname = usePathname();
@@ -48,7 +49,7 @@ export default function JobDetails() {
           {pathname === "/company-employee/jobs" && <Select />}
           {pathname === "/company-employee/jobs" && <MarkAsComplete />}
         </div>
-        {pathname === "/company-admin/jobs" &&
+        {pathname === "/company-admin/jobs" && (
           <div className="flex gap-2 h-3/4 mt-4 lg:mt-0">
             <div className="flex items-center bg-gray-100 rounded-xl px-3">
               <div className="bg-primary rounded-full h-2 w-2 mr-2"></div>
@@ -56,8 +57,8 @@ export default function JobDetails() {
             </div>
             <EditIcon />
             <DeleteIcon />
-          </div>}
-
+          </div>
+        )}
       </div>
       <div className="flex flex-col gap-1 mobile:text-left">
         <span className="font-bold text-lg">Description:</span>
@@ -93,7 +94,7 @@ export default function JobDetails() {
           <span className="bg-gray-100 rounded-2xl py-3 px-6 mt-2 md:text-base">
             $ 230.00
           </span>
-          </div>
+        </div>
         <div className="flex flex-col text-sm whitespace-nowrap">
           <span className="font-bold md:text-lg">Payment:</span>
           <span className="bg-gray-100 rounded-2xl py-3 px-6 mt-2 md:text-base">
@@ -113,7 +114,17 @@ export default function JobDetails() {
       </div>
       <div className="text-left">
         <span className="font-bold text-lg text-left">Onsite Progress:</span>
-        <TextEditor />
+        <div className="h-full border rounded-lg mt-2">
+          <Textarea
+            className="min-h-32"
+            placeholder="Write your onsite progress here."
+          />
+          <div className="flex items-end justify-end ">
+            <Button className="m-2 text-white" type="button">
+              Share
+            </Button>
+          </div>
+        </div>
         {/* Map comments */}
         {comments.map((comment, index) => (
           <Comment key={index} {...comment} />
