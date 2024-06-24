b0689b281704ddeb985098340169eae620dd2a71
abrauf18 <abrauf.dev@gmail.com>
2024-06-24 18:20:01 +0500

    feat: server action for jobs

--- actions/jobs/job-action.ts
+++ actions/jobs/job-action.ts
@@ -1,5 +1,6 @@
 "use server"
 import { auth } from "@/auth";
+import { JobsInterface } from "@/lib/interfaces";
 
 export const ServiceAction = async () => {
   const session = await auth();
@@ -15,3 +16,20 @@ export const ServiceAction = async () => {
   return result;
 };
 
+export const CreateJobAction = async (data: JobsInterface) => {
+  const session = await auth();
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/job/create`,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        //@ts-ignore
+        Authorization: `Bearer ${session?.token}`,
+      },
+      body: JSON.stringify(data),
+    }
+  );
+  const result = await response.json();
+  return result.data;
+};
