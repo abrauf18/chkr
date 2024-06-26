ddc10ab6ee9e4b13a1f0357d3e9eff10476248d4
abrauf18 <abrauf.dev@gmail.com>
2024-06-26 18:32:01 +0500

    feat: add skeleton loader for job cards

---
+++ components/shared/skeleton-loader.tsx
@@ -0,0 +1,14 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
+export function SkeletonLoader() {
+  return (
+    <div className="flex flex-col space-y-3 bg-white mt-4 p-4 rounded-3xl">
+      <Skeleton className="h-[130px] w-full bg-gray-200 rounded-xl" />
+      <div className="space-y-2">
+        <Skeleton className="h-4 w-[250px] bg-gray-200" />
+        <Skeleton className="h-4 w-[200px] bg-gray-200" />
+      </div>
+    </div>
+  );
+}
+
