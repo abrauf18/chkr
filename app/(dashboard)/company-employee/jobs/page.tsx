8c72b0854b02ac890d7ff3ba42b5ba9afa85f471
abrauf18 <abrauf.dev@gmail.com>
2024-06-26 12:26:33 +0500

    feat: api call at server side

--- app/(dashboard)/company-employee/jobs/page.tsx
+++ app/(dashboard)/company-employee/jobs/page.tsx
@@ -2,17 +2,19 @@ import MyJobs from '@/components/modules/company-employee/jobs/my-jobs'
 import React from 'react'
 import { Metadata } from 'next';
 import DashboardHeader from '@/components/shared/dashboard-header';
+import { GetUserJobsAction } from '@/actions/jobs/job-action';
 
 export const metadata: Metadata = {
   title: "My Jobs",
   description: "Manage all of your jobs efficiently."
 };
 
-export default function page() {
+export default async function page() {
+  const jobs = await GetUserJobsAction();
   return (
     <>
       <DashboardHeader title="My Jobs" />
-      <MyJobs/> 
+      <MyJobs jobs = {jobs}/> 
       </>
   );
 }
