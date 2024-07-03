0ec8d66b11719d98792214a8e06f8693815fa0e5
abrauf18 <abrauf.dev@gmail.com>
2024-07-03 16:05:24 +0500

    feat: server action to get companies data

---
+++ actions/company/company-action.ts
@@ -0,0 +1,21 @@
+'use server';
+
+import { auth } from "@/auth";
+
+export const GetCompanyAction = async () => {
+  const session = await auth();
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/company`, {
+    method: "GET",
+    headers: {
+      "content-type": "application/json",
+      // @ts-ignore
+      Authorization: `Bearer ${session?.token}`,
+    },
+    next: {
+      tags: ["getCompanies"],
+    },
+  });
+  const result = await response.json();
+  return result;
+};
+
