2e16637555d00e0f80ee7b4f6c68e0466e22fe42
abrauf18 <abrauf.dev@gmail.com>
2024-07-08 13:13:41 +0500

    feat: use context hook to use company data globally

---
+++ components/modules/super-admin/companies/companydata-context.tsx
@@ -0,0 +1,47 @@
+'use client'
+import React, { createContext, useContext, useState, useEffect, ReactNode } from 'react';
+import { GetCompanyAction } from '@/actions/company/company-action';
+import { CompanyInterface } from '@/lib/interfaces';
+
+interface CompanyDataContextProps {
+  data: CompanyInterface[] | null;
+  loading: boolean;
+  error: Error | null;
+}
+const CompanyDataContext = createContext<CompanyDataContextProps | undefined>(undefined);
+
+// a provider component
+export const CompanyDataProvider = ({ children }: { children: ReactNode }) => {
+  const [data, setData] = useState<CompanyInterface[] | null>(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await GetCompanyAction();
+        setData(response.data);
+      } catch (err) {
+        setError(err as Error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <CompanyDataContext.Provider value={{ data, loading, error }}>
+      {children}
+    </CompanyDataContext.Provider>
+  );
+};
+
+// Create a custom hook to use the CompanyDataContext
+export const useCompanyData = () => {
+  const context = useContext(CompanyDataContext);
+  if (context === undefined) {
+    throw new Error('useCompanyData must be used within a CompanyDataProvider');
+  }
+  return context;
+};
