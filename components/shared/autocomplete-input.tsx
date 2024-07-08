1b4d43aa0bbd9b4eec1a04267c6566992c2228aa
abrauf18 <abrauf.dev@gmail.com>
2024-07-08 10:31:05 +0500

    fix: styling of input field

--- components/shared/autocomplete-input.tsx
+++ components/shared/autocomplete-input.tsx
@@ -37,9 +37,10 @@ const GooglePlacesAutocomplete: React.FC<Props> = ({ name, placeholder }) => {
       {/* <Input ref={inputRef} type="text" placeholder={placeholder} className="pl-10 bg-[#F9F8F8]"/> */}
       <input ref={inputRef} type="text" placeholder={placeholder} 
       
-      className="pl-10 flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:black focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"/>
+      className="pl-10 bg-[#F9F8F8] flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:black focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"/>
     </>
   );
 };
 
 export default GooglePlacesAutocomplete;
+
