409a2dbabcf4fb4249f55d519e392736d8baa7ec
abrauf18 <abrauf.dev@gmail.com>
2024-07-10 17:17:34 +0500

    feat: updated feedback card attributes according to backend

--- components/modules/super-admin/feedback/feedback-card.tsx
+++ components/modules/super-admin/feedback/feedback-card.tsx
@@ -1,20 +1,31 @@
-import React from 'react';
+import React from "react";
 import ReviewStar from "@/assets/icons/star";
-import Image from 'next/image';
+import Image from "next/image";
 
 interface ReviewCardProps {
   name: string;
-  avatar: string;
+  url: string;
   rating: number;
   time: string;
-  reviewHeading: string;
   review: string;
 }
 
-const FeedbackCard: React.FC<ReviewCardProps> = ({ name, avatar, rating, time, reviewHeading, review }) => {
+const FeedbackCard: React.FC<ReviewCardProps> = ({
+  name,
+  url,
+  rating,
+  time,
+  review,
+}) => {
   return (
     <div className="p-4 border rounded-lg shadow-md flex space-x-4 bg-white">
-      <Image width={5} height={5} src='/images/avatar.svg' alt='user-img' className="w-12 h-12 rounded-full" />
+      <Image
+        width={33}
+        height={33}
+        src={url}
+        alt="user-img"
+        className="w-12 h-12 rounded-full"
+      />
       <div className="flex-1">
         <div className="flex items-center justify-between">
           <h3 className="text-lg font-semibold">{name}</h3>
@@ -22,10 +33,13 @@ const FeedbackCard: React.FC<ReviewCardProps> = ({ name, avatar, rating, time, r
         </div>
         <div className="flex items-center gap-1 mt-2">
           {[...Array(5)].map((_, i) => (
-            <ReviewStar key={i} fill={i < rating ? "#FFC107" : "#D9D9D9"} className="w-4 h-4" />
+            <ReviewStar
+              key={i}
+              fill={i < rating ? "#FFC107" : "#D9D9D9"}
+              className="w-4 h-4"
+            />
           ))}
         </div>
-        <h4 className="font-semibold mt-3">{reviewHeading}</h4>
         <p className="text-gray-600 mt-2 text-sm">{review}</p>
       </div>
     </div>
@@ -33,3 +47,4 @@ const FeedbackCard: React.FC<ReviewCardProps> = ({ name, avatar, rating, time, r
 };
 
 export default FeedbackCard;
+
