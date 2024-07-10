5f651ff020da7fb798a9f980006308c3d5d47e57
abrauf18 <abrauf.dev@gmail.com>
2024-07-10 15:03:32 +0500

    feat: integration of POST api for feedback

--- components/shared/feedback-form.tsx
+++ components/shared/feedback-form.tsx
@@ -6,6 +6,9 @@ import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { FeedbackSchema } from "@/lib/types";
 import { ErrorMessage } from "@hookform/error-message";
+import { FeedbackInterface } from "@/lib/interfaces";
+import { CreateFeedbackAction } from "@/actions/feedback/feedback-action";
+import { toast } from "react-toastify";
 
 export default function FeedbackForm() {
   const {
@@ -14,6 +17,8 @@ export default function FeedbackForm() {
     formState: { errors },
   } = useForm({
     resolver: zodResolver(FeedbackSchema),
+    mode: "onChange",
+    reValidateMode: "onChange",
   });
 
   const [rating, setRating] = useState(0);
@@ -22,17 +27,37 @@ export default function FeedbackForm() {
     setRating(newRating);
   };
 
-  const onSubmit = handleSubmit((data) => {
-    console.log(data);
-  });
+  const onSubmit = async (data: any) => {
+    try {
+      const messageData: FeedbackInterface = {
+        rating: rating, // Use rating from state
+        comment: data.comment,
+      };
+
+      console.log("Message data:", messageData);
+
+      const result = await CreateFeedbackAction(messageData);
+
+      console.log("Result:", result);
+
+      if (result && result.statusCode === 201) {
+        return toast.success(result.message);
+      } else {
+        return toast.error(result?.message);
+      }
+    } catch (error) {
+      console.error("Error sending feedback:", error);
+      toast.error("An error occurred while creating feedback.");
+    }
+  };
 
   return (
-    <form onSubmit={onSubmit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className="flex flex-col text-left justify-start lg:whitespace-nowrap mx-auto w-full">
         <h1 className="text-black mobile:text-lg md:text-xl text-semibold">
           How was your experience?
         </h1>
-        <span className="md:text-base xl:text-lg  text-neutral-700">
+        <span className="md:text-base xl:text-lg text-neutral-700">
           Thank you for choosing to provide feedback. Please rate your
           experience from 1 to 5 stars
         </span>
@@ -40,12 +65,14 @@ export default function FeedbackForm() {
       <div className="flex justify-center items-center w-full my-6">
         <StarRating rating={rating} onChange={handleRatingChange} />
       </div>
+      <p className="text-red-500 text-sm">
+        <ErrorMessage errors={errors} name="rating" />
+      </p>
       <div className="flex flex-col text-left justify-start">
         <h1 className="text-black text-semibold mobile:text-lg md:text-xl">
           Add your comments as feedback below.
         </h1>
-        <span className="md:text-base xl:text-lg lg:whitespace-nowrap text-neutral-700	">
-          {" "}
+        <span className="md:text-base xl:text-lg lg:whitespace-nowrap text-neutral-700">
           Your thoughts and suggestions are highly appreciated and help us
           improve our services.
         </span>
@@ -67,7 +94,7 @@ export default function FeedbackForm() {
         <Button className="w-full rounded-2xl bg-gray-300 text-black">
           Cancel
         </Button>
-        <Button className="w-full rounded-2xl text-white">
+        <Button className="w-full rounded-2xl text-white" type="submit">
           Submit Feedback
         </Button>
       </div>
