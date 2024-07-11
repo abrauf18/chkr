3bb5be2ce5ce2506477679eef164985297de2ad5
abrauf18 <abrauf.dev@gmail.com>
2024-07-11 15:24:56 +0500

    feat: server action for delete company

--- actions/company/company-action.ts
+++ actions/company/company-action.ts
@@ -24,10 +24,10 @@ export const GetCompanyAction = async () => {
   return result;
 };
 
-export const DiableCompanyAction = async (data: DisableCompanyInterface) => {
+export const DisableCompanyAction = async (data: DisableCompanyInterface) => {
   const session = await auth();
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/company/disable-comapny`,
+    `${process.env.NEXT_PUBLIC_API_URL}/company/disable-company`,
     {
       method: "PUT",
       headers: {
@@ -36,6 +36,26 @@ export const DiableCompanyAction = async (data: DisableCompanyInterface) => {
         Authorization: `Bearer ${session?.token}`,
       },
       body: JSON.stringify(data),
+    }
+  );
+  const result = await response.json();
+  if (result.statusCode === 401) {
+    redirect("/logout");
+  }
+  return result;
+};
+
+export const DeleteCompanyAction = async (id: number) => {
+  const session = await auth();
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/company/delete-company/${id}`,
+    {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        //@ts-ignore
+        Authorization: `Bearer ${session?.token}`,
+      },
     }
   );
   const result = await response.json();
