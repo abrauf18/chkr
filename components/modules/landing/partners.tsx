e171d74e190debdcac874e5ebfae04feddebec2b
abrauf18 <abrauf.dev@gmail.com>
2024-07-15 17:10:37 +0500

    feat: add slider animation to partners section

--- components/modules/landing/partners.tsx
+++ components/modules/landing/partners.tsx
@@ -7,7 +7,7 @@ import icon4 from "@/public/images/partner4.svg";
 import icon5 from "@/public/images/partner5.svg";
 import icon6 from "@/public/images/partner6.svg";
 
-const Partners = () => {
+const Partners: React.FC = () => {
   return (
     <div className="flex flex-col lg:py-10 mx-6 md:mx-16 items-center gap-4 mb-24">
       <h1 className="xl:font-semibold xl:text-4xl md:font-semibold md:text-3xl text-center">
@@ -16,37 +16,100 @@ const Partners = () => {
       <p className="xl:text-2xl lg:text-xl md:text-lg md:font-medium text-center text-gray-400">
         Trusted by top industry partners for seamless employee management.
       </p>
-      <div className="flex gap-x-4 md:gap-x-8">
-        <Image
-          src={icon1}
-          alt="Partner 1"
-          className="xl:w-[9rem] xl:h-[9rem] lg:w-[7rem] lg:h-[7rem]  w-[3rem] h-[3rem]"
-        />
-        <Image
-          src={icon2}
-          alt="Partner 2"
-          className="xl:w-[9rem] xl:h-[9rem] lg:w-[7rem] lg:h-[7rem]  w-[3rem] h-[3rem]"
-        />
-        <Image
-          src={icon3}
-          alt="Partner 3"
-          className="xl:w-[9rem] xl:h-[9rem] lg:w-[7rem] lg:h-[7rem]  w-[3rem] h-[3rem]"
-        />
-        <Image
-          src={icon4}
-          alt="Partner 4"
-          className="xl:w-[9rem] xl:h-[9rem] lg:w-[7rem] lg:h-[7rem]  w-[3rem] h-[3rem]"
-        />
-        <Image
-          src={icon5}
-          alt="Partner 5"
-          className="xl:w-[9rem] xl:h-[9rem] lg:w-[7rem] lg:h-[7rem]  w-[3rem] h-[3rem]"
-        />
-        <Image
-          src={icon6}
-          alt="Partner 6"
-          className="xl:w-[9rem] xl:h-[9rem] lg:w-[7rem] lg:h-[7rem]  w-[3rem] h-[3rem]"
-        />
+      <div className="scroll-container">
+        <div className="scroll-content flex gap-10">
+          <Image
+            src={icon1}
+            alt="Partner 1"
+            className="partner-image xl:w-[9rem] xl:h-[9rem] lg:w-[7rem] lg:h-[7rem]  w-[3rem] h-[3rem]"
+          />
+          <Image
+            src={icon2}
+            alt="Partner 2"
+            className="partner-image xl:w-[9rem] xl:h-[9rem] lg:w-[7rem] lg:h-[7rem]  w-[3rem] h-[3rem]"
+          />
+          <Image
+            src={icon3}
+            alt="Partner 3"
+            className="partner-image xl:w-[9rem] xl:h-[9rem] lg:w-[7rem] lg:h-[7rem]  w-[3rem] h-[3rem]"
+          />
+          <Image
+            src={icon4}
+            alt="Partner 4"
+            className="partner-image xl:w-[9rem] xl:h-[9rem] lg:w-[7rem] lg:h-[7rem]  w-[3rem] h-[3rem]"
+          />
+          <Image
+            src={icon5}
+            alt="Partner 5"
+            className="partner-image xl:w-[9rem] xl:h-[9rem] lg:w-[7rem] lg:h-[7rem]  w-[3rem] h-[3rem]"
+          />
+          <Image
+            src={icon6}
+            alt="Partner 6"
+            className="partner-image xl:w-[9rem] xl:h-[9rem] lg:w-[7rem] lg:h-[7rem]  w-[3rem] h-[3rem]"
+          />
+          {/* Duplicate images for seamless scroll */}
+          <Image
+            src={icon1}
+            alt="Partner 1"
+            className="partner-image xl:w-[9rem] xl:h-[9rem] lg:w-[7rem] lg:h-[7rem]  w-[3rem] h-[3rem]"
+          />
+          <Image
+            src={icon2}
+            alt="Partner 2"
+            className="partner-image xl:w-[9rem] xl:h-[9rem] lg:w-[7rem] lg:h-[7rem]  w-[3rem] h-[3rem]"
+          />
+          <Image
+            src={icon3}
+            alt="Partner 3"
+            className="partner-image xl:w-[9rem] xl:h-[9rem] lg:w-[7rem] lg:h-[7rem]  w-[3rem] h-[3rem]"
+          />
+          <Image
+            src={icon4}
+            alt="Partner 4"
+            className="partner-image xl:w-[9rem] xl:h-[9rem] lg:w-[7rem] lg:h-[7rem]  w-[3rem] h-[3rem]"
+          />
+          <Image
+            src={icon5}
+            alt="Partner 5"
+            className="partner-image xl:w-[9rem] xl:h-[9rem] lg:w-[7rem] lg:h-[7rem]  w-[3rem] h-[3rem]"
+          />
+          <Image
+            src={icon6}
+            alt="Partner 6"
+            className="partner-image xl:w-[9rem] xl:h-[9rem] lg:w-[7rem] lg:h-[7rem]  w-[3rem] h-[3rem]"
+          />
+          <Image
+            src={icon1}
+            alt="Partner 1"
+            className="partner-image xl:w-[9rem] xl:h-[9rem] lg:w-[7rem] lg:h-[7rem]  w-[3rem] h-[3rem]"
+          />
+          <Image
+            src={icon2}
+            alt="Partner 2"
+            className="partner-image xl:w-[9rem] xl:h-[9rem] lg:w-[7rem] lg:h-[7rem]  w-[3rem] h-[3rem]"
+          />
+          <Image
+            src={icon3}
+            alt="Partner 3"
+            className="partner-image xl:w-[9rem] xl:h-[9rem] lg:w-[7rem] lg:h-[7rem]  w-[3rem] h-[3rem]"
+          />
+          <Image
+            src={icon4}
+            alt="Partner 4"
+            className="partner-image xl:w-[9rem] xl:h-[9rem] lg:w-[7rem] lg:h-[7rem]  w-[3rem] h-[3rem]"
+          />
+          <Image
+            src={icon5}
+            alt="Partner 5"
+            className="partner-image xl:w-[9rem] xl:h-[9rem] lg:w-[7rem] lg:h-[7rem]  w-[3rem] h-[3rem]"
+          />
+          <Image
+            src={icon6}
+            alt="Partner 6"
+            className="partner-image xl:w-[9rem] xl:h-[9rem] lg:w-[7rem] lg:h-[7rem]  w-[3rem] h-[3rem]"
+          />
+        </div>
       </div>
     </div>
   );
