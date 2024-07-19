0f1c0796277fcdf3341e75f669e554e73bc6785e
abrauf18 <abrauf.dev@gmail.com>
2024-07-19 10:38:22 +0500

    fix: resolved build issue by adding sort and order in jobs api params

--- app/(dashboard)/company-admin/dashboard/@jobs/page.tsx
+++ app/(dashboard)/company-admin/dashboard/@jobs/page.tsx
@@ -3,8 +3,15 @@ import Jobs from "@/components/modules/company-admin/jobs/Jobs";
 import clsx from "clsx";
 import React from "react";
 
-async function JobsPage({ isDashboard }: { isDashboard?: boolean }) {
-  const jobs = await GetJobsAction();
+async function JobsPage({
+  isDashboard,
+  searchParams = { order: "defaultOrder", sort: "defaultSort" },
+}: {
+  isDashboard?: boolean;
+  searchParams?: { order: string; sort: string };
+}) {
+  const { order, sort } = searchParams;
+  const jobs = await GetJobsAction({ order, sort });
   return (
     <>
       <div className={clsx(isDashboard && "mb-6")}>
