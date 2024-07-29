0828345c3c89387d32bd772235b4b04684ea496a
abrauf18 <abrauf.dev@gmail.com>
2024-07-29 13:05:46 +0500

    fix : hydration error on client side

--- app/(dashboard)/company-employee/layout.tsx
+++ app/(dashboard)/company-employee/layout.tsx
@@ -1,5 +1,18 @@
+import { UserInfoAction } from "@/actions/settings/settings-action";
 import { auth } from "@/auth";
+import dynamic from "next/dynamic";
+
 import { redirect } from "next/navigation";
+const UpdateSession = dynamic(
+  () => import("@/components/modules/company-admin/dashboard/update-session"),
+  {
+    ssr: false,
+  }
+);
+
+const Stripe = dynamic(() => import("@/components/shared/stripe"), {
+  ssr: false,
+});
 
 export default async function RootLayout({
   children,
@@ -12,6 +25,18 @@ export default async function RootLayout({
   }
   const role = session?.user?.role;
   if (role === "company-employee") {
+    if (session?.user?.stripe_connect_account_id === null) {
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+      const result = await UserInfoAction();
+      if (result && result?.stripe_connect_account_id != null) {
+        return (
+          <UpdateSession
+            stripe_connect_account_id={result?.stripe_connect_account_id}
+          />
+        );
+      }
+      return <Stripe />;
+    }
     return <main>{children}</main>;
   }
   if (role === "admin" || role === "super-admin") {
