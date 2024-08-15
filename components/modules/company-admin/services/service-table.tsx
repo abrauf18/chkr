6435d2acb1c1dd2af93f6dba1bf44ca7305ddd96
abrauf18 <abrauf.dev@gmail.com>
2024-08-15 18:22:23 +0500

    feat: created service table and mapped data in it

---
+++ components/modules/company-admin/services/service-table.tsx
@@ -0,0 +1,120 @@
+"use client";
+import React, { useState } from "react";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+import {
+  Pagination,
+  PaginationItem,
+  PaginationContent,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+import { ArrowDown } from "lucide-react";
+import Image from "next/image";
+import DeleteModal from "../../../shared/delete-modal";
+// import EditAdmin from "./edit-admin";
+import DisableModal from "../../../shared/disable-modal";
+import { Services } from "@/lib/interfaces";
+import { ITEMS_PER_PAGE } from "@/lib/utils";
+
+const ServiceTable = ({
+  services,
+  currentPage,
+  setCurrentPage,
+  totalPageCount,
+  currentServices,
+}: {
+  services: Services[];
+  currentPage: number;
+  setCurrentPage: React.Dispatch<React.SetStateAction<number>>;
+  totalPageCount: number;
+  currentServices: Services[];
+}) => {
+  const handlePagination = (pageNumber: number) => {
+    if (pageNumber < 1 || pageNumber > totalPageCount) return;
+    setCurrentPage(pageNumber);
+  };
+  return (
+    <div>
+      <Table>
+        <TableHeader>
+          <TableRow className="bg-white">
+            <TableHead>
+              <div className="flex items-center gap-2">
+                <span className="text-black font-semibold whitespace-nowrap">
+                  Service Id
+                </span>
+                <ArrowDown className="h-4 w-4" />
+              </div>
+            </TableHead>
+            <TableHead>
+              <div className="flex items-center gap-2">
+                <span className="text-black font-semibold"> Service Name</span>
+                <ArrowDown className="h-4 w-4" />
+              </div>
+            </TableHead>
+            <TableHead>
+              <div className="flex items-center gap-2">
+                <span className="text-black font-semibold">Action</span>
+                <ArrowDown className="h-4 w-4" />
+              </div>
+            </TableHead>
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {currentServices.map((service, index) => (
+            <TableRow key={service.id}>
+              <TableCell>
+                <div className="flex items-center">
+                  <span className="whitespace-nowrap">{index + 1}</span>
+                </div>
+              </TableCell>
+              <TableCell>{service.service_name}</TableCell>
+              <TableCell>
+                <div className="flex gap-2">
+                  {/* <EditAdmin
+                    isAdmin={true}
+                    isEmployee={false}
+                    currentUser={admin}
+                  /> */}
+                  <DeleteModal userId={service.id} />
+                </div>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+      {services.length > ITEMS_PER_PAGE && (
+        <Pagination className="flex w-full justify-center items-center mt-4 bg-white rounded-2xl p-2 my-3">
+          <PaginationContent>
+            <PaginationItem
+              onClick={() => handlePagination(currentPage - 1)}
+            ></PaginationItem>
+            {Array.from({ length: totalPageCount }, (_, i) => (
+              <PaginationItem key={i + 1}>
+                <PaginationLink onClick={() => handlePagination(i + 1)}>
+                  {i + 1}
+                </PaginationLink>
+              </PaginationItem>
+            ))}
+            <PaginationItem>
+              <PaginationNext
+                onClick={() => handlePagination(currentPage + 1)}
+              />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      )}
+    </div>
+  );
+};
+
+export default ServiceTable;
+
