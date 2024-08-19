b2b7dd40a917ce6bb850322921e3c83819662beb
abrauf18 <abrauf.dev@gmail.com>
2024-08-19 17:11:27 +0500

    fix: auto location issue

--- components/shared/auto-location.tsx
+++ components/shared/auto-location.tsx
@@ -1,11 +1,10 @@
 "use client";
 
-import React, { useState } from "react";
+import React from "react";
 import { useFormContext } from "react-hook-form";
 import Script from "next/script";
 import Autocomplete from "react-google-autocomplete";
 import { MapPinned } from "lucide-react";
-import Loader from "./loader";
 
 interface Props {
   name: string;
@@ -13,7 +12,6 @@ interface Props {
 
 const AutoLocation: React.FC<Props> = ({ name }) => {
   const { setValue, watch } = useFormContext();
-  const [isLoading, setIsLoading] = useState(false);
 
   const onPlaceSelectedHandler = (place: any) => {
     if (!place.geometry) return;
@@ -24,37 +22,20 @@ const AutoLocation: React.FC<Props> = ({ name }) => {
       lng: place.geometry.location.lng(),
     };
     setValue(name, newAddress);
-    setIsLoading(false);
   };
 
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const inputValue = event.target.value;
-    if (inputValue.length > 0) {
-      setIsLoading(true);
-    } else {
-      setIsLoading(false);
-    }
-  };
-
-  const handleScriptLoad = () => {
-    console.log("Google Maps script loaded successfully");
-  };
-
-  const handleScriptError = () => {
-    setIsLoading(false);
-    console.error("Failed to load Google Maps script");
-  };
+  React.useEffect(() => {
+    setTimeout(() => (document.body.style.pointerEvents = ""), 0);
+  });
 
   return (
     <>
       <Script
         src={`https://maps.googleapis.com/maps/api/js?key=AIzaSyA3zX2wfM59W4JRCgja_k7Mbup0wuUXPTw&libraries=places`}
         strategy="beforeInteractive"
-        onLoad={handleScriptLoad}
-        onError={handleScriptError}
       />
       <div className="flex flex-col w-full">
-        <div className="relative w-full">
+        <div className="relative w-full ">
           <span className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400">
             <MapPinned className="h-5 w-5" color="#636363" />
           </span>
@@ -65,15 +46,9 @@ const AutoLocation: React.FC<Props> = ({ name }) => {
               types: ["geocode", "establishment"],
             }}
             className="pl-10 p-2 w-full border border-gray-300 rounded-md bg-[#F9F8F8]"
-            defaultValue={watch(name)?.name}
-            onChange={handleInputChange}
+            defaultValue={watch(name).name}
           />
         </div>
-        {isLoading && (
-          <div className="flex justify-center items-center py-2">
-            <div /> <Loader />
-          </div>
-        )}
       </div>
     </>
   );
